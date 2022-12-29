36019b3f7f77451773c4876c00f0ac6ca5b536fa
numangit <abdulnumanjob@gmail.com>
2022-12-29 06:18:11 +0600

    task details has been added

--- src/Pages/MyTasks/MyTasks.js
+++ src/Pages/MyTasks/MyTasks.js
@@ -11,28 +11,28 @@ const MyTasks = () => {
             <div>
                 {/* card */}
                 <div className='grid grid-cols-1 lg:grid-cols-3 gap-8 p-5 lg:p-10'>
-                    <div class="max-w-sm p-2 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <div className="max-w-sm p-2 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
                         {/* name and description */}
                         <div className='p-3 text-left'>
-                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Task Title here</h5>
-                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Task Description Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis qui esse repellendus quasi doloribus ipsa labore!</p>
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Task Title here</h5>
+                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Task Description Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis qui esse repellendus quasi doloribus ipsa labore!</p>
                         </div>
                         {/* buttons */}
-                        <div class="inline-flex justify-center w-full " role="group">
+                        <div className="inline-flex justify-center w-full " role="group">
                             <Link to="/taskDetails">
-                                <button type="button" class=" py-2 px-2 text-sm font-semibold text-gray-600 bg-white rounded-l-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-500 focus:z-10 focus:ring-2 focus:ring-blue-500 focus:text-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white flex items-center">
+                                <button type="button" className=" py-2 px-2 text-sm font-semibold text-gray-600 bg-white rounded-l-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-500 focus:z-10 focus:ring-2 focus:ring-blue-500 focus:text-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white flex items-center">
                                     <CgDetailsMore />&#160;Details
                                 </button>
                             </Link>
                             <Link to="/completed">
-                                <button type="button" class=" py-2 px-2 text-sm font-semibold text-gray-600 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-green-500 focus:z-10 focus:ring-2 focus:ring-green-500 focus:text-green-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-green-500 dark:focus:text-white flex items-center">
+                                <button type="button" className=" py-2 px-2 text-sm font-semibold text-gray-600 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-green-500 focus:z-10 focus:ring-2 focus:ring-green-500 focus:text-green-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-green-500 dark:focus:text-white flex items-center">
                                     <GoCheck />&#160;complete
                                 </button>
                             </Link>
-                            <button type="button" class=" py-2 px-2 text-sm font-semibold text-gray-600 bg-white border-l border-t border-b border-gray-200 hover:bg-gray-100 hover:text-orange-500 focus:z-10 focus:ring-2 focus:ring-orange-500 focus:text-orange-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-orange-500 dark:focus:text-white flex items-center">
+                            <button type="button" className=" py-2 px-2 text-sm font-semibold text-gray-600 bg-white border-l border-t border-b border-gray-200 hover:bg-gray-100 hover:text-orange-500 focus:z-10 focus:ring-2 focus:ring-orange-500 focus:text-orange-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-orange-500 dark:focus:text-white flex items-center">
                                 <GrEdit />&#160;Edit
                             </button>
-                            <button type="button" class=" py-2 px-2 text-sm font-semibold text-gray-600 bg-white rounded-r-md border border-gray-200 hover:bg-gray-100 hover:text-red-500 focus:z-10 focus:ring-2 focus:ring-red-500 focus:text-red-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-red-500 dark:focus:text-white flex items-center">
+                            <button type="button" className=" py-2 px-2 text-sm font-semibold text-gray-600 bg-white rounded-r-md border border-gray-200 hover:bg-gray-100 hover:text-red-500 focus:z-10 focus:ring-2 focus:ring-red-500 focus:text-red-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-red-500 dark:focus:text-white flex items-center">
                                 <AiFillDelete />&#160;Delete
                             </button>
                         </div>
--- src/Pages/TaskDetails/TaskDetails.js
+++ src/Pages/TaskDetails/TaskDetails.js
@@ -2,9 +2,27 @@ import React from 'react';
 
 const TaskDetails = () => {
     return (
-        <div className='mt-20 lg:h-screen'>
-            <h1>This is Task Details</h1>
-        </div>
+        <div className='mt-20 lg:h-screen p-2'>
+            <h1 className='text-2xl text-center my-3'>
+                <span className='text-amber-500 font-semibold'>Your</span> task details.
+            </h1>
+
+            <div class="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row first-letter:hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 lg:w-4/5 mx-auto">
+                <img
+                    class="m-2 object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-lg" src="https://media.istockphoto.com/id/1220731936/photo/high-angle-view-of-to-do-list-on-notepad-on-orange-background.jpg?s=612x612&w=0&k=20&c=YchB2TsZNujjeWHJKw_kU67ZGR1guJSabgXzYM4VDMs="
+                    alt="" />
+                <div class="flex flex-col justify-between p-4 leading-normal">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Task Title</h5>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Task Description : Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque doloribus impedit accusamus ea, perspiciatis dicta.</p>
+                    <div className='flex justify-evenly'>
+                        <span>Date:373737</span>
+                        <span>Date:373737</span>
+                        <span>Date:373737</span>
+                    </div>
+                </div>
+            </div>
+
+        </div >
     );
 };
 
