c271e9f7de75a85cd32a534366a0eb6f33d9097d
numangit <abdulnumanjob@gmail.com>
2023-06-04 00:46:55 +0600

    subscribe section complete

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Heading from './Heading/Heading';
 import Testimonial from './Testimonial/Testimonial';
+import Subscribe from './Subscribe/Subscribe';
 
 const Home = () => {
     return (
         <>
             <Heading></Heading>
             <Testimonial></Testimonial>
+            <Subscribe></Subscribe>
         </>
     );
 };
