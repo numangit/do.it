a78ccb8aa6a698833664184486a949f950c06f89
numangit <abdulnumanjob@gmail.com>
2023-06-04 17:40:18 +0600

    subscribe section input fixed

--- src/Pages/Home/Subscribe/Subscribe.js
+++ src/Pages/Home/Subscribe/Subscribe.js
@@ -9,10 +9,10 @@ const Subscribe = () => {
           <p className="mt-6 text-gray-300">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero similique obcaecati illum mollitia.
           </p>
-          <div className="w-full max-w-sm mx-auto mt-6 bg-transparent border rounded-md dark:border-gray-700 focus-within:border-blue-400 focus-within:ring focus-within:ring-blue-300 dark:focus-within:border-blue-300 focus-within:ring-opacity-40">
+          <div className="w-full max-w-sm mx-auto mt-6 bg-transparent border rounded-md dark:border-gray-700 bg-white focus-within:border-orange-400 focus-within:ring focus-within:ring-orange-300 dark:focus-within:border-orange-300 focus-within:ring-opacity-40">
             <form className="flex flex-col md:flex-row">
-              <input type="email" placeholder="Enter your email address" className="flex-1 h-10 px-4 py-2 m-1 text-gray-700 placeholder-gray-400 bg-transparent border-none appearance-none dark:text-gray-200 focus:outline-none focus:placeholder-transparent focus:ring-0" />
-              <button type="button" className="h-10 px-4 py-2 m-1 text-white transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400">
+              <input type="email" placeholder="Enter your email address" className="flex-1 h-10 px-4 py-2 m-1 placeholder-gray-100 bg-transparent border-none appearance-none text-gray-200 focus:outline-none focus:placeholder-transparent focus:ring-0" />
+              <button type="button" className="h-10 px-4 py-2 m-1 text-white transition-colors duration-300 transform bg-orange-500 rounded-md hover:bg-orange-400 focus:outline-none focus:bg-orange-400">
                 Join Us
               </button>
             </form>
