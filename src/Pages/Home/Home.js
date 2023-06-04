09215702c74085f2ee69a1ba09e16766bb6b36ba
numangit <abdulnumanjob@gmail.com>
2023-06-04 16:10:07 +0600

    partners componen added to home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Heading from './Heading/Heading';
 import Testimonial from './Testimonial/Testimonial';
 import Subscribe from './Subscribe/Subscribe';
+import Partners from './Partners/Partners';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Heading></Heading>
             <Testimonial></Testimonial>
             <Subscribe></Subscribe>
+            <Partners></Partners>
         </>
     );
 };
