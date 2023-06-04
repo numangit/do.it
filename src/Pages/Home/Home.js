d2b18b49c48c5d2023bbc4397584b3ca324f9f66
numangit <abdulnumanjob@gmail.com>
2023-06-04 16:25:18 +0600

    services created and added to home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,11 +3,13 @@ import Heading from './Heading/Heading';
 import Testimonial from './Testimonial/Testimonial';
 import Subscribe from './Subscribe/Subscribe';
 import Partners from './Partners/Partners';
+import Services from './Services/Services';
 
 const Home = () => {
     return (
         <>
             <Heading></Heading>
+            <Services></Services>
             <Testimonial></Testimonial>
             <Subscribe></Subscribe>
             <Partners></Partners>
