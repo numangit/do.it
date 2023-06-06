08078585d7b94649beb0bf86c2923062cfdb3166
numangit <abdulnumanjob@gmail.com>
2023-06-06 20:27:29 +0600

    highlight component content added

--- src/Pages/Home/Higlights/Higlights.js
+++ src/Pages/Home/Higlights/Higlights.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import templates from "../../../assets/Screenshot (242).png";
 
 const Higlights = () => {
 
@@ -7,15 +8,15 @@ const Higlights = () => {
       <div class="container flex flex-col px-6 py-10 mx-auto space-y-6 lg:h-[32rem] lg:py-16 lg:flex-row lg:items-center">
         <div class="w-full lg:w-1/2">
           <div class="lg:max-w-lg">
-            <h1 class="text-3xl font-semibold tracking-wide text-gray-800 dark:text-white lg:text-4xl">Find your premium new glasses exported from US</h1>
-            <p class="mt-4 text-gray-600 dark:text-gray-300">We work with the best remunated glasses dealers in US to find your new glasses.</p>
+            <h1 class="text-3xl font-semibold tracking-wide text-gray-800 dark:text-white lg:text-4xl">Templates designed for business teams</h1>
+            <p class="mt-4 text-gray-600 dark:text-gray-300">Create new projects in seconds from dozens of carefully crafted templates that span every team</p>
             <div class="grid gap-6 mt-8 sm:grid-cols-2">
               <div class="flex items-center text-gray-800 -px-3 dark:text-gray-200">
                 <svg class="w-5 h-5 mx-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
                 </svg>
 
-                <span class="mx-3">Premium selection</span>
+                <span class="mx-3">Human resources</span>
               </div>
 
               <div class="flex items-center text-gray-800 -px-3 dark:text-gray-200">
@@ -23,7 +24,7 @@ const Higlights = () => {
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
                 </svg>
 
-                <span class="mx-3">Insurance</span>
+                <span class="mx-3">Operations</span>
               </div>
 
               <div class="flex items-center text-gray-800 -px-3 dark:text-gray-200">
@@ -31,7 +32,7 @@ const Higlights = () => {
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
                 </svg>
 
-                <span class="mx-3">All legal documents</span>
+                <span class="mx-3">Design</span>
               </div>
 
               <div class="flex items-center text-gray-800 -px-3 dark:text-gray-200">
@@ -39,7 +40,7 @@ const Higlights = () => {
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
                 </svg>
 
-                <span class="mx-3">From US glasses dealers</span>
+                <span class="mx-3">Finance</span>
               </div>
 
               <div class="flex items-center text-gray-800 -px-3 dark:text-gray-200">
@@ -47,7 +48,7 @@ const Higlights = () => {
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
                 </svg>
 
-                <span class="mx-3">Payment Security</span>
+                <span class="mx-3">Legal</span>
               </div>
 
               <div class="flex items-center text-gray-800 -px-3 dark:text-gray-200">
@@ -55,14 +56,14 @@ const Higlights = () => {
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
                 </svg>
 
-                <span class="mx-3">Fast shipping (+ Express)</span>
+                <span class="mx-3">Sales</span>
               </div>
             </div>
           </div>
         </div>
 
         <div class="flex items-center justify-center w-full h-96 lg:w-1/2">
-          <img class="object-cover w-full h-full max-w-2xl rounded-md" src="https://images.unsplash.com/photo-1555181126-cf46a03827c0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="glasses" />
+          <img class="object-cover w-full h-full max-w-2xl rounded-md" src={templates} alt="glasses" />
         </div>
       </div>
     </header >
