62440b71e7f42951f591a2a0234af83e19c71fb7
numangit <abdulnumanjob@gmail.com>
2023-06-06 21:04:30 +0600

    minial changes made

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -30,7 +30,7 @@ const Navbar = () => {
                                     <button onClick={handleLogOut} type="button" className="text-white bg-orange-500 hover:bg-orange-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700">Sign Out</button>
                                 </Link>
                                 : <Link to="/signin">
-                                    <button type="button" className="text-white bg-orange-600 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700">Sign In</button>
+                                    <button type="button" className="text-white  bg-orange-500 hover:bg-orange-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700">Sign In</button>
                                 </Link>
                         }
 
