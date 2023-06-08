ef0c5941190d7bbed3e86494780af5a5dc86083e
numangit <abdulnumanjob@gmail.com>
2023-06-08 21:51:03 +0600

    footer changed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,32 +1,49 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { AuthContext } from '../Contexts/AuthProvider';
-import logo from '../assets/logo.png';
 
 const Footer = () => {
-    const { user } = useContext(AuthContext);
     return (
         <footer class="bg-white dark:bg-gray-900">
-            <div class="container flex flex-col items-center justify-between px-6 py-8 mx-auto lg:flex-row">
-                <Link to="/" className="flex items-center">
-                    <img src={logo} className="h-6 mr-3 sm:h-9" alt="do.it Logo" />
-                    <span className="self-center text-xl whitespace-nowrap dark:text-white text-blue-800 font-logo font-extrabold">DO.iT</span>
-                </Link>
+            <div className="border-t border-slate-200 bg-slate-100 py-4 text-sm">
+                <div className="container mx-auto px-6">
+                    <div className="flex items-center justify-center">
+                        <nav
+                            className="col-span-3 md:col-span-4 lg:col-span-6"
+                            aria-labelledby="subfooter-links-5-logo-sub"
+                        >
+                            <h3 className="sr-only" id="subfooter-links-5-logo-sub">
+                                Get in touch
+                            </h3>
+                            <ul className="flex flex-wrap items-center justify-end gap-2 lg:gap-4">
+                                <li className="leading-6">
+                                    <Link
+                                        to="/"
+                                        className="transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600"
+                                    >
+                                        T&C
+                                    </Link>
+                                </li>
+                                <li className="leading-6">
+                                    <Link
+                                        to="/"
+                                        className="transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600"
+                                    >
+                                        Privacy
+                                    </Link>
+                                </li>
+                                <li className="leading-6">
+                                    <Link
+                                        to="/"
 
-                <div class="flex flex-wrap items-center justify-center gap-4 mt-6 lg:gap-6 lg:mt-0">
-                    {
-                        user?.uid &&
-                        <>
-                            <Link to="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-sm">Home</Link>
-                            <Link to="/addTask" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-sm">Add Task</Link>
-                            <Link to="/myTasks" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-sm">My Tasks</Link>
-                            <Link to="/completed" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-sm">Completed</Link>
-
-                        </>
-                    }
+                                        className="transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600"
+                                    >
+                                        Cookies
+                                    </Link>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
                 </div>
-
-                <p class="mt-6 text-sm text-gray-500 lg:mt-0 dark:text-gray-400">© Copyright 2023 Do.it </p>
             </div>
         </footer>
     );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -17,7 +17,7 @@ const Navbar = () => {
 
     return (
         <>
-            <nav className="bg-slate-300/30 backdrop-blur-lg px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0 ">
+            <nav className="bg-slate-300/30 backdrop-blur-xl px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0 ">
                 <div className="container flex flex-wrap items-center justify-between mx-auto">
                     <Link to="/" className="flex items-center">
                         <img src={logo} className="h-6 mr-3 sm:h-9" alt="do.it Logo" />
@@ -42,19 +42,19 @@ const Navbar = () => {
                     <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
                         <ul className="flex flex-col p-2 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li>
-                                <Link to="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-lg font-semibold">Home</Link>
+                                <Link to="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-base font-normal">Home</Link>
                             </li>
                             {
                                 user?.uid &&
                                 <>
                                     <li>
-                                        <Link to="/addTask" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-lg font-semibold">Add Task</Link>
+                                        <Link to="/addTask" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-base font-normal">Add Task</Link>
                                     </li>
                                     <li>
-                                        <Link to="/myTasks" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-lg font-semibold">My Tasks</Link>
+                                        <Link to="/myTasks" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-base font-normal">My Tasks</Link>
                                     </li>
                                     <li>
-                                        <Link to="/completed" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-lg font-semibold">Completed</Link>
+                                        <Link to="/completed" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-base font-normal">Completed</Link>
                                     </li>
                                 </>
                             }
